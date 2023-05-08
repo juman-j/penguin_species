55a1a219c4eda6e0a264b8d027a9654dd01d3905
Artem Sorokin <85310022+juman-j@users.noreply.github.com>
2023-05-08 17:06:51 +0200

    Entire project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='4it439 skolni projekt tym1',
+    author='tym1',
+    license='',
+)
